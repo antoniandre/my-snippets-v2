b82f5eabcb1ae824f4238b617d7cc8c1fc225282
Antoni André <antoniandre.web@gmail.com>
2018-09-29 19:33:28 +1000

    Cleanup app root layout.

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,4 +5,10 @@ import 'vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify, {
   customProperties: true,
   iconfont: 'md',
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }
 })
