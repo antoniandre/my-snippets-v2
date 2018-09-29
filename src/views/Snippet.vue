ffdccb2b7332a5e9e6594dfb1a3209393f786ada
Antoni André <antoniandre.web@gmail.com>
2018-09-29 19:34:28 +1000

    Create a snippet details view.

---
+++ src/views/Snippet.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  div.snippet
+    h1 Snippet {{ snippet.name }}
+    p Some content. id: {{ id }}
+    v-btn(:to="{ name: 'home' }" small)
+      v-icon.mr-2 keyboard_arrow_left
+      | Back to list
+</template>
+
+<script>
+export default {
+  props: [ 'id' ],
+  data: () => ({
+    snippet: {
+      name: 'First Snippet'
+    }
+  })
+}
+</script>
+
