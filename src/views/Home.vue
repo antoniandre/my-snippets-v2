548e9c95493b9d5cd3179aa5da480f22e24271c5
Antoni André <antoniandre.web@gmail.com>
2018-09-29 19:33:59 +1000

    Create a grid of snippets.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,17 @@
-<template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+<template lang="pug">
+  v-container(fluid)
+    v-slide-y-transition(mode="out-in")
+      v-container(grid-list-sm fluid)
+        v-layout(row wrap)
+          v-flex(v-for="n in 16" :key="n" xs3 sm2 md2 lg1 d-flex)
+            v-card.d-flex(flat tile)
+              router-link(to="/snippet/test")
+                v-img(:src="`https://picsum.photos/250/150?image=${n * 5 + 10}`"
+                      :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+                      aspect-ratio="1"
+                      class="grey lighten-2")
+                  v-layout(slot="placeholder" fill-height align-center justify-center ma-0)
+                    v-progress-circular(indeterminate color="grey lighten-5")
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
