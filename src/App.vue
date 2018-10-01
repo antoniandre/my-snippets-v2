80d1007673e2071a01ee2651b3e8923759e6c168
Antoni André <antoniandre.web@gmail.com>
2018-10-01 23:00:48 +1000

    Improve general layout.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  v-app
+  v-app.white
     v-toolbar.elevation-0(app dense color="white")
       v-toolbar-title
         router-link(to="/")
@@ -16,7 +16,7 @@
           v-list-tile-action
             v-icon compare_arrows
           v-list-tile-title Switch drawer (click me)
-    v-footer.py-3.text-xs-center.justify-center(color="grey lighten-4")
+    v-footer.py-3.text-xs-center.justify-center.grey--text(color="white")
       span.font-italic.caption &copy; 2018 Antoni Andre. All rights reserved.
 </template>
 
@@ -36,5 +36,9 @@ export default {
 <style lang="scss">
 a {
   text-decoration: none;
+}
+
+.v-toolbar {
+  border-bottom: 1px solid #f2f2f2 !important;
 }
 </style>
