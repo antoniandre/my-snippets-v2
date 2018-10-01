9b9fa099d934ce548549997fbd2c62a68677c10d
Antoni André <antoniandre.web@gmail.com>
2018-10-01 13:42:56 +1000

    Add vue-multipane and improve snippet page layout.

--- src/views/Snippet.vue
+++ src/views/Snippet.vue
@@ -1,20 +1,183 @@
 <template lang="pug">
-  div.snippet
-    h1 Snippet {{ snippet.name }}
-    p Some content. id: {{ id }}
-    v-btn(:to="{ name: 'home' }" small)
-      v-icon.mr-2 keyboard_arrow_left
-      | Back to list
+  v-card.ma-5.pa-3.snippet
+    v-layout.mb-4(align-center)
+      v-icon.mr-3(large color="grey lighten-1") star
+      h1
+        v-text-field.headline.snippet__title(:class="{ focus: focused.title }" :value="snippet.name" append-icon="edit" hide-details solo flat @focus="focused.title = true" @blur="focused.title = false")
+      v-spacer
+      span(@click="verticalLayout = !verticalLayout")
+        span.mr-2.subheading.grey--text Change layout
+        v-btn(color="primary" icon)
+          v-icon {{ verticalLayout ? 'view_stream' : 'view_column' }}
+
+    p.font-italic(v-html="snippet.description")
+    p
+      strong.mr-2.subheading id:
+      v-text-field.d-inline-block.subheading.snippet__id(:class="{ focus: focused.id }" :value="id" append-icon="edit" hide-details solo flat @focus="focused.id = true" @blur="focused.id = false")
+
+    multipane.outter(:layout="verticalLayout ? 'vertical' : 'horizontal'")
+      multipane(:layout="verticalLayout ? 'horizontal' : 'vertical'")
+        v-textarea.pane.pane--source(box label="HTML" v-model="snippet.source.html" hide-details)
+        multipane-resizer
+        div.pane.pane--source(box auto-grow label="CSS" v-model="snippet.source.css") div 2
+        multipane-resizer
+        div.flex.pane.pane--source(box auto-grow label="CSS" v-model="snippet.source.js") div 3
+      multipane-resizer
+      div.subheading.flex.pane(v-html="snippet.source.html")
+
+    //- v1.
+    //- multipane(:layout="verticalLayout ? 'vertical' : 'horizontal'")
+      v-card.pane.first(flat :style="{ width: verticalLayout ? '50%' : '100%' }")
+        v-card-title.subheading
+          v-icon.mr-2 code
+          | Source
+        v-card-text
+          multipane(:layout="verticalLayout ? 'horizontal' : 'vertical'")
+            div.pane.first(:style="{ width: verticalLayout ? '100%' : '50%' }") pane 1
+            multipane-resizer
+            div.flex.pane pane 2
+
+          //- v-textarea(box auto-grow label="Some source code." v-model="snippet.source")
+          //- v-layout
+            v-spacer
+            v-btn.mt-0.mx-0(color="primary" small round depressed)
+              v-icon.mr-1 check
+              | Save changes
+      multipane-resizer
+      v-card.flex.pane(flat)
+        v-card-title.subheading Result
+        v-card-text(v-html="snippet.source")
+
+    v-card-actions
+      router-link.mt-4(:to="{ name: 'home' }")
+        v-btn.mr-2(color="primary" icon)
+          v-icon chevron_left
+        span.subheading.grey--text Back to list
 </template>
 
 <script>
+import { Multipane, MultipaneResizer } from 'vue-multipane'
+
 export default {
   props: [ 'id' ],
+  components: { Multipane, MultipaneResizer },
   data: () => ({
     snippet: {
-      name: 'First Snippet'
+      name: 'First Snippet',
+      description: 'Some content.',
+      source: {
+        html: '<p><strong>Lorem ipsum dolor sit amet consectetur adipisicing elit.</strong></p>',
+        css: '',
+        js: ''
+      }
+    },
+    compiled: null,
+    verticalLayout: true,
+    focused: {
+      title: false,
+      id: false
     }
   })
 }
 </script>
 
+<style lang="scss">
+$primary: #42b983;
+
+.v-card__actions a {
+  text-decoration: none;
+}
+
+.v-text-field.v-text-field--solo .v-input__control {min-height: 2em;}
+
+.snippet__title,
+.snippet__id {
+  .v-icon {
+    opacity: 0;
+    font-size: 1.3em;
+  }
+
+  .v-input__slot {
+    padding: 0 !important;
+    border-bottom: 1px solid transparent;
+  }
+
+  &:hover,
+  &.focus {
+    .v-icon {
+      opacity: 1;
+    }
+
+    .v-input__slot {
+      border-bottom-color: #ddd;
+    }
+  }
+
+  &.focus .v-input__slot {
+    border-bottom-color: $primary;
+  }
+}
+
+.multipane {
+  &.layout-v {
+    height: 400px;
+
+    > .multipane-resizer {margin-left: 0;left: 0;}
+  }
+
+  &.layout-h {
+    height: 100%;
+
+    > .multipane-resizer {margin-top: 0;top: 0;}
+  }
+
+  > .pane {
+    overflow: hidden;
+    background: #f8f8f8;
+    box-shadow: inset 0 0 4px rgba(#000, 0.1);
+  }
+
+  > .multipane-resizer {
+    position: relative;
+    background: #fff;
+    z-index: 2;
+
+    &:before {
+      content: "";
+      position: absolute;
+      display: block;
+      width: 4px;
+      height: 40px;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border: 1px solid #ccc;
+      border-width: 0 1px;
+      transition: 0.3s ease;
+
+    }
+
+    &:hover:before {
+      border-color: #999;
+    }
+  }
+
+  &.layout-h > .multipane-resizer:before {
+    width: 40px;
+    height: 4px;
+    border-width: 1px 0;
+  }
+}
+
+.outter.layout-h {height: 400px;}
+.layout-v .layout-h {width: 50%;}
+.layout-h .layout-v {height: 50%;}
+.layout-h .layout-v {width: 100% !important;}
+.layout-v .layout-h {height: 100% !important;}
+.layout-v .layout-h > .pane {width: 100% !important;}
+.layout-h .layout-v > .pane {height: 100% !important;}
+.layout-v .pane--source {height: 33%;}
+.layout-h .pane--source {width: 33%;}
+
+.v-input__control, .v-input__slot {height: 100% !important;}
+</style>
